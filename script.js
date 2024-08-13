7f0335711080ab56555bc1aaefb872628201de24
heon2100 <91506032+heon2100@users.noreply.github.com>
2024-08-14 00:25:51 +0900

    Update script.js

--- script.js
+++ script.js
@@ -63,18 +63,6 @@ document.querySelector(".op1").innerHTML = p[3];
 
 
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
 
 
 
