cf6ccab29f842d562d5e445e211f6ff3fb30cb9a
heon2100 <91506032+heon2100@users.noreply.github.com>
2024-08-15 21:43:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,6 @@ function inputCheck(){
 span.onclick = function() {
   modal.style.display = "none";
 }
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
